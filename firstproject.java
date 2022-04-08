9f687bd3192ce2315205db4eae0a84c40e2b1063
patrick <pgenon53@gmail.com>
2022-04-07 21:43:01 -0600

    FirstProject

---
+++ firstproject.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+class firstproject {
+
+    public static void main(String[] args) {
+
+        int[] lottery_numbers = new int[49];
+        int i = 0;
+
+        for (i = 0; i < lottery_numbers.length; i++)
+            ;
+        {
+            lottery_numbers[i] = i + 1;
+            System.out.println(lottery_numbers[i]);
+        }
+    }
+}
+// System.out.println("your age is 19");
+// } else if (Myage == 18) {
+// System.out.println("your age is 18");
+// } else if (Myage == 20) {
+// System.out.println("your age is 20");
+// } else {
+// System.out.println("i gave up i dont know your age");
+// }
+// switch (Myage) {
+// case 19:
+// System.out.println("your age is 19");
+// case 20:
+// System.out.println("your age is 20");
+// case 18:
+// System.out.println("your age is 18");
+
+// int sum = 0;
+
+// create an object of Scanner class
+// Scanner input = new Scanner(System.in);
+
+// take integer input from the user
+// System.out.println("Enter first number");
+// int number = input.nextInt();
+// user while be ask in age and checks if its greater than 60
+// while loop continues
+// until entered number is positive
+// while (number >= 0) {
+// add only positive numbers
+// sum += number;
+
+// System.out.println("Enter number");
+// number = input.nextInt();
+// }
+
+// System.out.println("Sum = " + sum);
+// input.close();
+
+// int count;
+// for( count =0; count)
+//
+// if (5 == 5) {
+// System.out.print("John doe");
+// System.out.print("laura");
+
+// Scanner nameScanner = new Scanner(System.in);
+
+// int age;
+
+// System.out.println("What is your name");
+// age = newScanner.nextInt();
+
+// if (age == 25) {
+// System.out.println("What is your name");
+// } else {
+// System.out.println("The age is given 25");
+// }
+// nameScanner.close();
